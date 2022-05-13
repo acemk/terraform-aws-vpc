f580599c03896a754b153ffe68e54278fe788614
AceMK <ace@sonete.com>
2022-05-13 15:07:48 +0200

    Fix for_each public subnet TG attachment

--- main.tf
+++ main.tf
@@ -1258,7 +1258,7 @@ resource "aws_default_vpc" "this" {
 # Transit Gateway VPC Attachment
 ################################################################################
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  for_each = var.aws_subnet.public
+  for_each = aws_subnet.public
 
   transit_gateway_id = var.tgw_id
   vpc_id             = aws_vpc.this[0].id
